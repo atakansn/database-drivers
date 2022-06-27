3d606a65e383633475af0f2bf9b117b3f038902d
atakansn <your_email@example.com>
2022-06-27 15:54:05 +0300

    Converted to abstract class and implement driver interface.

---
+++ src/Driver/PDO/ConnectorObject.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace DatabaseDrivers\Driver\PDO;
+
+use DatabaseDrivers\Driver\DriverInterface;
+use DatabaseDrivers\Exceptions\DriverExtensionNotFoundException;
+use DatabaseDrivers\Exceptions\PDOExtensionNotFoundException;
+use PDO;
+use PDOException;
+use Throwable;
+
+abstract class ConnectorObject implements DriverInterface
+{
+    protected array $options = [
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL,
+        PDO::ATTR_STRINGIFY_FETCHES => false,
+        PDO::ATTR_EMULATE_PREPARES => false
+    ];
+
+    public function createConnection(string $dsn, array $config, array $options)
+    {
+        [$userName, $password] = [
+            $config['username'] ?? null,
+            $config['password'] ?? null
+        ];
+
+        try {
+            return $this->createPdoConnection($dsn, $userName, $password, $options);
+        } catch (Throwable $t) {
+            throw new PDOException($t->getMessage(), (int)$t->getCode());
+        }
+    }
+
+    protected function createPdoConnection(string $dsn, string $username, string $password, array $options)
+    {
+        return new PDO($dsn, $username, $password, $options);
+    }
+
+    public function getOptions(array $config)
+    {
+        $options = $config['options'] ?? [];
+
+        return array_diff_key($this->options, $options) + $options;
+    }
+
+    public function getDefaultOptions()
+    {
+        return $this->options;
+    }
+
+    public function setDefaultOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    protected function extControl(...$extName)
+    {
+        $this->checkPDO();
+
+        foreach ($extName as $ext) {
+            if (!extension_loaded($ext)) {
+                throw new DriverExtensionNotFoundException("{$ext} extension not found. Please install {$ext} extension.");
+            }
+        }
+
+        return true;
+    }
+
+    protected function checkPDO()
+    {
+        if(!extension_loaded('pdo')) {
+            throw new PDOExtensionNotFoundException('PDO extension not found');
+        }
+
+        return true;
+    }
+
+    protected function getAvailableDrivers()
+    {
+        return PDO::getAvailableDrivers();
+    }
+
+
+}
